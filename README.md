6d59cf0126fd6e29971ed69f7a60ed8f12927866
eran Sevil <eransevil2@gmail.com>
2024-08-02 14:31:40 +0300

    end of server side

---
+++ README.md
@@ -0,0 +1,15 @@
+# Installation
+
+1. Install dependencies: `npm install`
+2. Start the server: `npm start`
+
+- Implemented session management and user state tracking
+- Developed slot machine roll logic with house advantage
+- Created endpoints for rolling slots and cashing out
+- Added error handling
+- Tested and validated all endpoints
+
+## Potential future features
+
+1. **Database Integration**: Store user sessions and game data in a real DB.
+2. **User authentication**: Add user registration and login functionality to track individual player stats.
--- api/game.service.js
+++ api/game.service.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const SYMBOLS = ["C", "L", "O", "W"];
 const REWARDS = {
   C: 10,
@@ -8,11 +9,84 @@ const REWARDS = {
 
 let sessions = require("../dbMock/sessions.json");
 
-const startGame = () => {};
+const startGame = () => {
+  const sessionId = _generateSessionId();
+  sessions[sessionId] = { credits: 10 };
+  _saveSessionsToFile();
+  return { sessionId, credits: 10 };
+};
 
-const spinSlot = (sessionId) => {};
+const spinSlot = (sessionId) => {
+  if (!sessions[sessionId]) {
+    throw new Error("Invalid session ID");
+  }
 
-const cashOut = (sessionId) => {};
+  let credits = sessions[sessionId].credits;
+  if (credits < 1) {
+    throw new Error("Insufficient credits");
+  }
+
+  credits -= 1;
+  sessions[sessionId].credits = credits;
+  let spinResult = _createSpin();
+  let winAmount = 0;
+
+  if (_isUserWon(spinResult)) {
+    winAmount = REWARDS[spinResult[0]];
+    credits += winAmount;
+  }
+  // re-spin logic
+  if (credits - winAmount >= 40) {
+    const reSpinProbability = credits < 60 ? 0.3 : 0.6;
+    if (Math.random() < reSpinProbability && winAmount > 0) {
+      const secondRoundResult = _createSpin();
+      if (!_isUserWon(secondRoundResult)) {
+        spinResult = secondRoundResult;
+        credits -= winAmount;
+      }
+    }
+  }
+
+  sessions[sessionId].credits = credits;
+  _saveSessionsToFile();
+  return { spinResult, credits };
+};
+
+const cashOut = (sessionId) => {
+  if (!sessions[sessionId]) {
+    throw new Error("Invalid session ID");
+  }
+
+  const credits = sessions[sessionId].credits;
+  _saveSessionsToFile();
+  delete sessions[sessionId];
+  return { credits };
+};
+
+const _generateSessionId = () => {
+  return "xxxxxx".replace(/x/g, () => ((Math.random() * 36) | 0).toString(36));
+};
+
+const _createSpin = () => {
+  return Array(3)
+    .fill()
+    .map(() => SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)]);
+};
+
+const _isUserWon = (array) => array.every((element) => element === array[0]);
+
+const _saveSessionsToFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(
+      "dbMock/sessions.json",
+      JSON.stringify(sessions, null, 2),
+      (err) => {
+        if (err) reject(err);
+        else resolve();
+      }
+    );
+  });
+};
 
 module.exports = {
   startGame,
